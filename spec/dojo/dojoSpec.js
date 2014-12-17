f0fbb5bc5c3c2fe31a102177ba9ef3d08d446d4b
DadoCe <dado.ce@gmail.com>
2014-12-17 09:14:21 -0300

    Rename refactoring

--- spec/dojo/dojoSpec.js
+++ spec/dojo/dojoSpec.js
@@ -1,35 +1,34 @@
 var Decisor = require('../../src/index');
 
 describe("Carinha da Goiaba:", function() {
-  var decisor = new Decisor();
   
   it("Se não tem grana, nao compra a goiaba", function() {
-    var dado = new Decisor(0.50, false); 
+    var decisor = new Decisor(0.50, false); 
 
-    expect(dado.decide()).toBe('');
+    expect(decisor.decide()).toBe('');
   });
 
   it("Tem fome e tem grana, compra a goiaba", function() {
-    var dado = new Decisor(2, true); 
+    var decisor = new Decisor(2, true); 
 
-    expect(dado.decide()).toBe('comprar goiaba');
+    expect(decisor.decide()).toBe('comprar goiaba');
   });
 
   it("Não tem fome e tem grana, não compra a goiaba e pega o onibus", function() {
-    var dado = new Decisor(2, false); 
+    var decisor = new Decisor(2, false); 
 
-    expect(dado.decide()).toBe('comprar passagem');
+    expect(decisor.decide()).toBe('comprar passagem');
   });
 
   it("Tem fome e não tem grana, não compra a goiaba", function() {
-    var dado = new Decisor(0.5, true); 
+    var decisor = new Decisor(0.5, true); 
 
-    expect(dado.decide()).toBe('');
+    expect(decisor.decide()).toBe('');
   });
 
   it("Compra a goiaba e pega onibus", function() {
-    var dado = new Decisor(10, true); 
-    
-    expect(dado.decide()).toBe('comprar goiaba e comprar passagem');
+    var decisor = new Decisor(10, true); 
+
+    expect(decisor.decide()).toBe('comprar goiaba e comprar passagem');
   });
 });
